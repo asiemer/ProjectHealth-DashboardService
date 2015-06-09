dbcbf2eaf54ff06e6aa6a720d9a3eddec9e7a709
Eric Fleming <eric-fleming18@hotmail.com>
2015-06-09 15:11:04 -0400

    Initial commit from i0

---
+++ src/Projects/Domain/Base/IRepository.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+namespace Projects.Domain
+{
+    public interface IRepository
+    {
+        T GetById<T>(Guid id) where T : class, IAggregate;
+        void Save(IAggregate aggregate);
+    }
+}
