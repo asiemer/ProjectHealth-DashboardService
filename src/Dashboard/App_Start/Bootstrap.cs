aef68331b70408903861d1f2e87fef775e8cf3d8
Eric Fleming <eric-fleming18@hotmail.com>
2015-06-17 12:02:25 -0400

    Moved NSB into Dashboard project and removed Dashboard.Handler project

--- src/Dashboard/App_Start/Bootstrap.cs
+++ src/Dashboard/App_Start/Bootstrap.cs
@@ -6,6 +6,7 @@ using Dashboard.Infrastructure;
 using Dashboard.ReadModel.Providers;
 using log4net;
 using log4net.Config;
+using NServiceBus;
 using StatsdClient;
 using StructureMap;
 using WebApiContrib.IoC.StructureMap;
@@ -19,6 +20,7 @@ namespace Dashboard
         public static void Init()
         {
             InitLogging();
+            InitNSB();
             ObjectFactory.Initialize(init =>
             {
                 init.For<IRepository>().Use(c => _repository);
@@ -37,6 +39,17 @@ namespace Dashboard
             };
 
             Metrics.Configure(metricsConfig);
+        }
+
+        private static void InitNSB()
+        {
+            var busConfiguration = new BusConfiguration();
+            busConfiguration.EndpointName("Dashboard.Dashboard");
+            busConfiguration.UseSerialization<XmlSerializer>();
+            busConfiguration.EnableInstallers();
+            busConfiguration.UsePersistence<InMemoryPersistence>();
+
+            Bus.Create(busConfiguration).Start();
         }
 
         private static void InitLogging()
