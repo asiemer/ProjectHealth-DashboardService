90f76dea659d8a55fb9fed9d52f40587e5e84ed7
Eric Fleming <eric-fleming18@hotmail.com>
2015-06-22 10:58:46 -0400

    Extracted logic into a MongoDBModifier

--- src/Dashboard/Controllers/DashboardController.cs
+++ src/Dashboard/Controllers/DashboardController.cs
@@ -1,13 +1,9 @@
 ﻿using System;
-using System.Net;
-using System.Net.Http;
 using System.Threading.Tasks;
 using System.Web.Http;
 using Dashboard.Extentions;
-using Dashboard.Infrastructure;
 using Dashboard.Models.RAGWidgetModels;
 using Dashboard.ReadModel.Providers;
-using Dashboard.ReadModel.Views;
 
 namespace Dashboard.Controllers
 {
@@ -28,21 +24,6 @@ namespace Dashboard.Controllers
             
             var ragWidgetFromDb = await _ragWidgetViewProvider.Get(ragWidgetId);
             return ragWidgetFromDb.ToRagWidgetInfo();
-        }
-
-        [HttpPost]
-        [Route("create")]
-        public HttpResponseMessage CreateRAGWidgetInformation(CreateRAGWidgetRequest req)
-        {
-            //This is just here temporary until we figure out how we want to handle incoming events.
-            //var _applicationSettings = new ApplicationSettings();
-            //var writer = new MongoDbProjectionWriter<RAGWidgetView>(_applicationSettings.MongoDbConnectionString, _applicationSettings.MongoDbName);
-            //var id = new Guid("533e1569-c6f6-4771-acdf-aeeca2ec064c");
-            //var dashboardView = new RAGWidgetView { Id = id, Yellow = 0, Green = 0, Red = 0 };
-            //writer.Add(id, dashboardView);
-            //new Test().CreateRecord();
-
-            return Request.CreateResponse(HttpStatusCode.Created, "RAG Information Created in Dashboard DB");
         }
     }
 }
--- src/Dashboard/Handlers/UpdateProjectScoreHandler.cs
+++ src/Dashboard/Handlers/UpdateProjectScoreHandler.cs
@@ -1,27 +1,23 @@
 ﻿using System;
 using System.Threading.Tasks;
 using Dashboard;
-using Dashboard.Infrastructure;
 using Dashboard.ReadModel.Providers;
-using Dashboard.ReadModel.Views;
 using NServiceBus;
 using StructureMap;
 
 public class UpdateProjectScoreHandler : IHandleMessages<UpdateProjectScore>
 {
-    private readonly IProjectionWriter<RAGWidgetView> _commandWriter;
     private readonly IRAGWidgetViewProvider _ragWidgetViewProvider;
     IBus bus;
         
     public UpdateProjectScoreHandler(IBus bus)
-        : this(bus, ObjectFactory.GetInstance<IProjectionWriter<RAGWidgetView>>(), ObjectFactory.GetInstance<IRAGWidgetViewProvider>())
+        : this(bus, ObjectFactory.GetInstance<IRAGWidgetViewProvider>())
     {
         this.bus = bus;
     }
 
-    public UpdateProjectScoreHandler(IBus bus, IProjectionWriter<RAGWidgetView> commandWriter, IRAGWidgetViewProvider ragWidgetViewProvider)
-    {
-        _commandWriter = commandWriter;
+    public UpdateProjectScoreHandler(IBus bus, IRAGWidgetViewProvider ragWidgetViewProvider)
+    {;
         _ragWidgetViewProvider = ragWidgetViewProvider;
     }
 
@@ -32,37 +28,7 @@ public class UpdateProjectScoreHandler : IHandleMessages<UpdateProjectScore>
 
     private async Task HandleAsync(UpdateProjectScore command)
     {
-        UpdateDatabase(command);
-    }
-
-    private async void UpdateDatabase(UpdateProjectScore command)
-    {
-        var dbitem = await CheckIfRAGWidgetExists(command.Id);
-        var exists = (dbitem != null);
-
-        var ragWidgetView = new RAGWidgetView
-        {
-            Green = command.Green,
-            Yellow = command.Yellow,
-            Red = command.Red,
-        };
-        
-        if (exists)
-        {
-            new Test().UpdateRecord(command);
-        }
-        else
-        {
-            new Test().CreateRecord(command, ragWidgetView);
-        }
-    }
-
-
-
-    private async Task<RAGWidgetView> CheckIfRAGWidgetExists(Guid id)
-    {
-        RAGWidgetView dbItem =  await _ragWidgetViewProvider.GetById(id);
-        return dbItem;
+        new MongoDbModifier(_ragWidgetViewProvider).UpdateDatabase(command);
     }
 }
 
